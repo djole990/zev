14b572497e174e6f8e4756918a788a045c857efc
unknown <djordjije.radulovic@gmail.com>
2020-01-31 10:47:44 +0100

    initial project setup

---
+++ src/main/java/com/djole/zev/ZevApplication.java
@@ -0,0 +1,13 @@
+package com.djole.zev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djole/zev/ZevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djole.zev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
